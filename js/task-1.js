73f4c35988dda7b719733c23b417a65f75614aee
saregokcan <gokcansare@gmail.com>
2025-05-14 20:28:42 +0300

    son

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
 
-  if (totalPrice <= customerCredits) {
-    console.log(`You ordered <quantity> droids worth <totalPrice> credits!`)
+   if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
   } else {
-    console.log(`Insufficient funds!`)
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
